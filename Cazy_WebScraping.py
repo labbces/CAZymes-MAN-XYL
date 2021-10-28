dd7abe6c49e7473234c358bce691ba2acf46fabc
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2021-10-28 19:05:11 -0300

    renaming file

--- Cazy_WebScraping.py
+++ Cazy_WebScraping.py
@@ -12,10 +12,12 @@ print(table)'''
 # Using pandas_html to parse from web
 #df = pd.read_html('http://www.cazy.org/GH1_structure.html',skiprows=1)
 
-#using local html to avoid problems with cazy
+#using local html to avoid problems with with cazy 
 file_path = 'cazy.html'
 with open(file_path, 'r') as f:
     df = pd.read_html(f.read(), skiprows=1)
 
 structure_table = df[1]
 structure_table.to_csv('gh1_structure.cvs',index='false')
+new_table = structure_table.iloc[:-1,:]
+new_table.to_csv("gh1_structure.csv")

0d629827515c315ddfa54f79ed0b60dbd4b99e6e
Diego Mauricio Riaño-Pachón <diriano@gmail.com>
2021-12-08 01:45:15 -0300

    spelling

--- DB/downloadGenomeFiles.py
+++ DB/downloadGenomeFiles.py
@@ -4,7 +4,7 @@ parser= argparse.ArgumentParser(description='Download genomes from NCBI')
 parser.add_argument('--password', metavar='password', type=str, help='password for the database')
 parser.add_argument('--dirPath', dest='dirPath', type=str, help='folder where genome files shoudl be stored')
 parser.add_argument('--fileType', dest='fileType', type=str, 
-                    help='Wich type of gneome file shoudl be downloaded',
+                    help='Which type of genome file should be downloaded',
                     choices=['Genome sequence', 'Protein sequence', 'Genome annotation', 'Protein sequence alter'])
 args= parser.parse_args()
 

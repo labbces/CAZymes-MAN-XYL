e8da2706612a3979073032e64255e99931601732
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2022-06-28 21:45:37 -0300

    handling double support values and heatmap view improvement

--- scripts/ETE_phylogenies.py
+++ scripts/ETE_phylogenies.py
@@ -1,5 +1,5 @@
 # IMPORTS
-#from ete3 import Tree, NodeStyle, TreeStyle
+from ete3 import Tree, NodeStyle, TreeStyle
 import matplotlib.pyplot as plt
 from tokenize import group
 from ete3 import Tree, ClusterTree
@@ -7,14 +7,33 @@ import PyQt5
 import pandas as pd
 from pyparsing import matchPreviousExpr
 
+#t = Tree("curto.treefile", format=1)
+#t = Tree("62GH-afa.aln.treefile", format=1, quoted_node_names=True)
+#t = Tree("GH116.aln.tree", format=1)
+t = Tree("curto.tree", format=1, quoted_node_names=True)
 
-t = Tree("62GH-afa.aln.treefile", format=1, quoted_node_names=True)
 # print(t.write(format=9))
-tree = Tree(t.write(format=9), quoted_node_names=True)
+#tree = Tree(t.write(format=9), quoted_node_names=True)
 # print(tree)
 # sed 's/::/../g' 62GH-afa.aln.treefile  > 62GH_renamed-afa.aln.treefile
 #t = Tree("test.tree")
 info = {}
+
+for node in t.traverse("postorder"):
+    if not node.is_leaf():
+        try:
+            node.support = float(node.name.split("/")[0]) # value of the support using fastbootstrap
+            #node.support = float(node.name.split("/")[1])# value of the support using alrt
+            print(node.support)
+        except:
+            print("no bootstrap")
+
+#for node in t.traverse("postorder"):
+#    node.name = node.name.split("_")[0]
+
+#Getting midpoint outgroup for tree and rooting tree
+midpoint_outgroup = t.get_midpoint_outgroup()
+t.set_outgroup(midpoint_outgroup)
 
 for leaf in t.iter_leaves():
     # print(leaf.name)
@@ -56,14 +75,16 @@ for leaf in t.iter_leaves():
 
 matrix = pd.DataFrame.from_dict(info, orient='index')
 matrix2 = matrix.reset_index().rename({'index': '#Names'}, axis='columns')
-# print(matrix)
 matrix2.to_csv(r'matrix2.txt', header=True, index=False, sep="\t")
 
 
 print(f'Tree has {len(info.keys())} leaves/sequences')
 
-t = ClusterTree(tree.write(), text_array="matrix2.txt")
-
 # ver como colocar o que cada coluna significa
-t.show("heatmap")
-t.render(file_name='fileName.svg')
+t = ClusterTree(t.write(), text_array="matrix2.txt")
+
+# Setting Tree Style and Visualization
+ts = TreeStyle()
+ts.show_leaf_name = True
+ts.show_scale = True
+t.show("heatmap", tree_style=ts)

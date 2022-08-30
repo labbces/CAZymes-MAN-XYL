a818e643c0e80415a0f713abdb48649dce3e23e3
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2022-08-30 11:53:34 -0300

    Update

--- scripts/phylo_output.py
+++ scripts/phylo_output.py
@@ -1,6 +1,6 @@
 # IMPORTS
 import argparse
-from ete3 import Tree, PhyloTree, TreeStyle, ClusterTree, NodeStyle, faces, AttrFace, ProfileFace, TextFace
+from ete3 import Tree, TreeStyle
 
 # Using argparse to handle variables
 parser = argparse.ArgumentParser()
@@ -8,8 +8,8 @@ parser.add_argument('-i','--input', help="Treefile",type=str,required=True, meta
 parser.add_argument('-f','--filename', help="prefix of output files",type=str,required=True, metavar="")
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-n','--ninja', action="store_true",help="newick file from ninja")
-group.add_argument('-iqu','--iqtreefastbootstrap', action="store_true", help="Processe using fastbootstrap")
-group.add_argument('-iqa','--iqtreealrt', action="store_true",help="Processe using alrt")
+group.add_argument('-iqu','--iqultrabootstrap', action="store_true", help="Processe using fastbootstrap")
+group.add_argument('-iqa','--iqalrt', action="store_true",help="Processe using alrt")
 args = parser.parse_args()
 
 
@@ -26,9 +26,9 @@ if not args.ninja:
     for node in t.traverse("postorder"):
         if not node.is_leaf():
             try:
-                if args.iqu:
+                if args.iqultrabootstrap:
                     node.support = float(node.name.split("/")[0]) # value of the support using fastbootstrap
-                elif args.iqa:
+                elif args.iqalrt:
                     node.support = float(node.name.split("/")[1])# value of the support using alrt
             except:
                 continue
@@ -47,14 +47,22 @@ ts.show_scale = True
 ts.show_branch_length = True
 ts.show_branch_support = True
 #t.show(tree_style=ts)
-t.render(f"{args.filename}.pdf", tree_style=ts)
-t.render(f"{args.filename}.svg", tree_style=ts)
+try:
+    if args.iqultrabootstrap:
+       t.render(f"{args.filename}_withfastbootstrap.nwk.pdf", tree_style=ts)
+       t.render(f"{args.filename}_withfastbootstrap.nwk.svg", tree_style=ts)
+    elif args.iqalrt:
+         t.render(f"{args.filename}_withalrt.nwk.pdf", tree_style=ts)
+         t.render(f"{args.filename}_withalrt.nwk.svg", tree_style=ts)
+except:
+    pass
+
 
 # Writing the tree in newick format
 try:
-    if args.iqu:
-        t.write(format=1, outfile=f"{args.filename}_withfastbootstrap.nwk") 
-    elif args.iqa:
-        t.write(format=1, outfile=f"{args.filename}_withalrt.nwk")
+    if args.iqultrabootstrap:
+        t.write(format=0, outfile=f"{args.filename}_withfastbootstrap.nwk") 
+    elif args.iqalrt:
+        t.write(format=0, outfile=f"{args.filename}_withalrt.nwk")
 except:
     pass

c7ad311870f26a48760ba07ae5fa68d1ecaae00b
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2022-11-15 11:01:51 -0300

    Update

--- scripts/rmsd.R
+++ scripts/rmsd.R
@@ -1,28 +1,25 @@
 #!/usr/bin/env Rscript
 
 #Installing the necessary packages
-install.packages("bio3d", dependencies=TRUE)
-install.packages("iterpc", dependencies = TRUE)
-install.packages("tidyverse")
+#install.packages("bio3d", dependencies=TRUE)
+#install.packages("iterpc", dependencies = TRUE)
+#install.packages("tidyverse")
 #install.packages("argparser", dependencies = TRUE)
-#install.packages("iterators")
-#install.packages("foreach")
-#install.packages("doParallel")
+#install.packages("bio3d", repos="http://cran.r-project.org", dependencies=TRUE, lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
+#install.packages("iterpc", repos="http://cran.r-project.org", dependencies=TRUE, lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
+#install.packages("tidyverse", repos="http://cran.r-project.org", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
 
 #Loading packages
 library(bio3d) 
 library(tibble)
 library(iterpc)
 #library(argparser)
-#library(iterators)
-#library(foreach)
-#library(parallel)
-#library(doParallel)
-
+#library("bio3d", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase") 
+#library("tibble", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
+#library("iterpc", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
 
 #Iterating over the pdb files for each family
-files <- list.files(path="/home/dan/CAZymes-MAN-XYL/grupos/alphafold_gh31", pattern="\\.pdb", full.names = TRUE, recursive=FALSE)
-#files2 <- list.files(path="/home/dan/CAZymes-MAN-XYL/parsing_pdb/GH95", pattern="\\.pdb", recursive=FALSE)
+files <- list.files(path="/home/dan/CAZymes-MAN-XYL/grupos/alphafold_gh115", pattern="\\.pdb", full.names = TRUE, recursive=FALSE)
 
 # Using iterpc package to obtain distinct combinations
 J <- iterpc(table(files), 2, labels = files)
@@ -39,12 +36,12 @@ similarity <- double(0)
 # Looping to get vectors
 for(i in seq(iterations)) {
   combination <-  getnext(J)
-  pdb1 <- read.pdb(combination[1])
+  pdb1 <- read.pdb(combination[1], ATOM.only = TRUE)
   name1 <- strsplit(combination[1], "/")
   name1.1 <- strsplit(name1[[1]][length(name1[[1]])], "\\.")
   name1.2 <- name1.1[[1]][1]
   ids1 <- c(ids1, name1.2)
-  pdb2 <- read.pdb(combination[2])
+  pdb2 <- read.pdb(combination[2], ATOM.only = TRUE)
   name2 <- strsplit(combination[2], "/")
   name2.1 <- strsplit(name2[[1]][length(name1[[1]])], "\\.")
   name2.2 <- name2.1[[1]][1]
@@ -58,8 +55,4 @@ for(i in seq(iterations)) {
 # Writing Edge List
 data = tibble(id1 = ids1, id2 = ids2, similarity = similarity)
 
-write.table(data, file = "GH31.csv", sep="\t", row.names=FALSE, col.names = FALSE)
-
-#foreach(x=it) %do% {pdb1 = read.pdb(x[1]); pdb2 = read.pdb(x[2])} %do% #{ rmsd(a=x[1]$xyz, b=x[2]$xyz, fit=TRUE) }
-#foreach(x=it) %do% {print(x[2])}
-
+write.table(data, file = "GH115_atomonly.csv", sep="\t", row.names=FALSE, col.names = FALSE)

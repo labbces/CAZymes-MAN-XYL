6c9e0d6ac8dd69f5f6872a6a5338239709077f31
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2022-11-14 22:17:16 -0300

    Update

--- scripts/crawling_alphafold.py
+++ scripts/crawling_alphafold.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # IMPORTS
+import argparse
 import os
 from time import sleep
-import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
+# Handling arguments by command line
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", help="input with list of uniprot IDs", type=str, required=True)
+parser.add_argument("-o", "--output", help="path to write output files", type=str, required=True)
+args = parser.parse_args()
+
+# Creating directory if already not exists
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
 
 # Settign options for Chrome
 chrome_options = webdriver.ChromeOptions()
@@ -14,13 +25,13 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 navegador = webdriver.Chrome("/usr/local/bin/chromedriver", options=chrome_options)
 
-
 # Setting couting variables
 sucess = 0
-failed = 0
+fail = 0
+fail_list = []
 
 # Acessing AlphaFold with Selenium
-with open("uniprot-compressed_true_download_true_format_list-2022.11.12-22.29.34.11.list") as fhand:
+with open(args.input) as fhand:
   for line in fhand:
     try:
       line = line.rstrip()
@@ -28,16 +39,17 @@ with open("uniprot-compressed_true_download_true_format_list-2022.11.12-22.29.34
       sleep(1)
       pdbfile = navegador.find_element_by_xpath("/html/body/div/app-root/section/app-entry/div[1]/div/app-summary-text/div/div[1]/div[2]/a[1]")
       url = pdbfile.get_attribute('href')
-      os.system('wget %s' % url)
+      #os.system('wget %s' % url)
+      os.system(f'curl -o {args.output}/{line}.pdb {url}')
       sucess += 1
     except:
-      failed += 1
+      fail += 1
+      fail_list.append(line)
       pass
 
-print("Sucess: ", sucess, "Failed: ", failed)
-
-
+print("Sucess: ", sucess, "Failed: ", fail)
+with open(args.output+"/failed.txt", "w") as f:
+  for item in fail_list:
+    f.write(item+"\n")
+print("Failed list saved in: ", args.output+"/failed.txt")
 navegador.close()
-
-
-

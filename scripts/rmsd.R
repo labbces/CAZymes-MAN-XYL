5c8cc11bd3cb0951e76edab183aaca2186c67b3b
Danilo-Brito-Rocha <danilobrittorocha@gmail.com>
2022-11-19 22:28:14 -0300

    Command line habilitation for rmsd.R

--- scripts/rmsd.R
+++ scripts/rmsd.R
@@ -4,7 +4,6 @@
 #install.packages("bio3d", dependencies=TRUE)
 #install.packages("iterpc", dependencies = TRUE)
 #install.packages("tidyverse")
-#install.packages("parallel",dependencies = TRUE)
 #install.packages("argparser", dependencies = TRUE)
 #install.packages("bio3d", repos="http://cran.r-project.org", dependencies=TRUE, lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
 #install.packages("iterpc", repos="http://cran.r-project.org", dependencies=TRUE, lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
@@ -15,25 +14,32 @@ library(bio3d)
 library(tidyverse)
 library(iterpc)
 library(parallel)
-#library(argparser)
+library(argparser)
 #library("bio3d", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase") 
-#library("tibble", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
+#library("tidyverse", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
 #library("iterpc", lib="/Storage/data2/danilo.brito/CAZymes-MAN-XYL/DB/XylanDatabase")
 
+# Handling commands in terminal using argparse
+parser <- arg_parser(description = "RMSD calculation")
+parser <- add_argument(parser, "--input", help = "PDB files path")
+parser <- add_argument(parser, "--output", help = "Output file path")
+parser <- add_argument(parser, "--threads", help = "Number of threads")
+argsv <- parse_args(parser)
+
 #Setting ncores
-setup_core_value = as.integer(3)
-typeof(setup_core_value)
+setup_core_value <- as.integer(argsv$threads)
 setup.ncore(ncore=setup_core_value)
 
 #Iterating over the pdb files for each family
-files <- list.files(path="/home/dan/CAZymes-MAN-XYL/grupos/alphafold_gh115", pattern="\\.pdb", full.names = TRUE, recursive=FALSE)
+#files <- list.files(path="/home/dan/CAZymes-MAN-XYL/parsing_pdb/GH115", pattern="\\.pdb", full.names = TRUE, recursive=FALSE)
+files <- list.files(path=argsv$input, pattern="\\.pdb", full.names = TRUE, recursive=FALSE)
 
 # Using iterpc package to obtain distinct combinations
 J <- iterpc(table(files), 2, labels = files)
 
 # Counting number of iterations
 iterations <- nc_multiset(table(files), 2) # getlength(J) would work too
-print(iterations)
+cat("Number of combinations:", iterations)
 
 # Setting empty non-recursive vectors
 ids1 <- character(0)
@@ -41,30 +47,29 @@ ids2 <- character(0)
 return_rmsd <- double(0)
 
 # Looping to get vectors
-for(i in seq(iterations)) {
+for (i in seq(iterations)) {
   combination <-  getnext(J)
   pdb1 <- read.pdb(combination[1], ATOM.only = TRUE, verbose = FALSE)
   name1 <- strsplit(combination[1], "/")
-  name1.1 <- strsplit(name1[[1]][length(name1[[1]])], "\\.")
-  name1.2 <- name1.1[[1]][1]
-  ids1 <- c(ids1, name1.2)
+  name1_1 <- strsplit(name1[[1]][length(name1[[1]])], "\\.")
+  name1_2 <- name1_1[[1]][1]
+  ids1 <- c(ids1, name1_2)
   pdb2 <- read.pdb(combination[2], ATOM.only = TRUE, verbose = FALSE)
   name2 <- strsplit(combination[2], "/")
-  name2.1 <- strsplit(name2[[1]][length(name1[[1]])], "\\.")
-  name2.2 <- name2.1[[1]][1]
-  ids2 <- c(ids2, name2.2)
+  name2_1 <- strsplit(name2[[1]][length(name1[[1]])], "\\.")
+  name2_2 <- name2_1[[1]][1]
+  ids2 <- c(ids2, name2_2)
   rmsd = rmsd(a=pdb1$xyz,b=pdb2$xyz, fit=TRUE,  ncore=setup_core_value, nseg.scale=setup_core_value) 
-  #cat(name1.2, name2.2, return_rmsd, return_similarity,"\n")
   return_rmsd <- c(return_rmsd, rmsd)
 }
 
 # Converting distance in similarity
-max_distance_value = max(return_rmsd)
-min_distance_value = min(return_rmsd)
-rmsd_dataframe = tibble(return_rmsd)
+max_distance_value <- max(return_rmsd)
+min_distance_value <- min(return_rmsd)
+rmsd_dataframe <- tibble(return_rmsd)
 #similarity = tibble(apply(rmsd_dataframe, 1, function(x) {max_distance_value - x}))
-similarity = tibble(apply(rmsd_dataframe, 1, function(x) {(x - min_distance_value) / (max_distance_value - min_distance_value)}))
+similarity <- tibble(apply(rmsd_dataframe, 1, function(x) {(x - min_distance_value) / (max_distance_value - min_distance_value)}))
 
 # Writing Edge List
-data = tibble(id1 = ids1, id2 = ids2, similarity)
-write.table(data, file = "GH115_scaling_nseg_scale.csv", sep="\t", row.names=FALSE, col.names = FALSE)
+data <- tibble(id1 = ids1, id2 = ids2, similarity)
+write.table(data, file=argsv$output, sep="\t", row.names=FALSE, col.names = FALSE)
